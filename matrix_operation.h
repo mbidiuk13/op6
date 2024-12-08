5c3db5e26122f07402bbd0ee6315f305cbc16ccd
mbidiuk13 <masabiduk07@gmail.com>
2024-12-08 14:34:01 +0200

    Update matrix_operation.h

--- matrix_operation.h
+++ matrix_operation.h
@@ -1,5 +1,5 @@
-#ifndef MATRIX_SOLVER_H
-#define MATRIX_SOLVER_H
+#ifndef MATRIX_OPERATION_H
+#define MATRIX_OPERATION_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,77 +25,79 @@ void input_matrix_manually(double **A, double *b, int rows, int precision) {
         do {
             printf("Enter %d element(s) for row #%d\n", rows, i + 1);
 
-            // Вводимо елементи матриці для поточного рядка
+            //введення елементів матриці для поточного рядка
             for (int j = 0; j < rows; j++) {
                 A[i][j] = get_double_input("Enter value: ", -100, 100);
                 if (i == j) {
-                    diagonal_element = fabs(A[i][j]);  // Діагональний елемент
+                    diagonal_element = fabs(A[i][j]);
                 } else {
-                    off_diagonal_sum += fabs(A[i][j]);  // Сума елементів, що не належать до діагоналі
+                    off_diagonal_sum += fabs(A[i][j]);
                 }
             }
 
-            // Перевірка на діагональне домінування
+            //перевірка на діагональне домінування
             if (diagonal_element <= off_diagonal_sum) {
                 printf("Invalid row: A[%d][%d] = %.*lf is not greater than the sum of other elements = %.*lf.\n",
                        i + 1, i + 1, precision, diagonal_element, precision, off_diagonal_sum);
                 printf("Please re-enter the entire row #%d.\n", i + 1);
             }
-        } while (diagonal_element <= off_diagonal_sum);  // Якщо не виконано, повторюємо введення для цього рядка
+        } while (diagonal_element <= off_diagonal_sum);
 
-        // Вводимо вектор b
+        //введення вектор b
         printf("Enter b[%d]:\n", i + 1);
         b[i] = get_double_input("Enter value: ", -100, 100);
     }
 }
 
-// Solve SLAE using an iterative method
-int solve_system(double **matrix, double *vector, double *prev_sol, double *current_sol, int size, double epsilon) {
-    double max_diff = 0.0;
-    int iterations = 0, max_iterations = 1000;
-
-    // Ітерації методу
+//розв'язання слар, використовуючи ітеративний метод
+void solve_slae(double **a, const double *b, double *x, double *xp, const unsigned int size, const double epsilon) {
+    int iteration = 0;
+    double max_delta = 0.0;
     do {
+        iteration++;
+
         for (int i = 0; i < size; i++) {
-            double sum = 0.0;
+            xp[i] = 0.0;
             for (int j = 0; j < size; j++) {
-                if (j != i) sum += matrix[i][j] * prev_sol[j];
+                if (i != j) {
+                    xp[i] = xp[i] + a[i][j] * x[j];
+                }
             }
-            current_sol[i] = (vector[i] - sum) / matrix[i][i];
-
-            // Визначення максимальної зміни серед компонент вектора
-            double diff = fabs(current_sol[i] - prev_sol[i]);
-            if (diff > max_diff) max_diff = diff;
+            xp[i] = (b[i] - xp[i]) / a[i][i];
         }
 
-        // Оновлення попереднього розв'язку
-        for (int i = 0; i < size; i++) prev_sol[i] = current_sol[i];
-
-        iterations++;  // Лічильник ітерацій
-
-        // Перевірка на перевищення максимальної кількості ітерацій
-        if (iterations >= max_iterations) {
-            printf("The method did not converge within the maximum number of iterations (%d).\n", max_iterations);
-            return 0;  // Не зійшлося
+        for (int i = 0; i < size; i++) {
+            if (fabs(x[i] - xp[i]) > max_delta) {
+                max_delta = fabs(x[i] - xp[i]);
+            }
+            x[i] = xp[i];
         }
-    } while (max_diff > epsilon);  // Порівняння з точністю
 
-    // Якщо рішення зійшлося
-    printf("Converged in %d iterations.\n", iterations);
-    return 0;
+    } while (max_delta > epsilon);
+
+    printf("Converged in %d iterations.\n", iteration);
 }
 
-void verify_solution(double **A, double *b, int rows, int precision) {
-    printf("\nVerification of the solution (equations):\n");
+void verify_solution(double **A, double *x, int rows, int precision) {
+    printf("\nVerification of the solution:\n");
+
     for (int i = 0; i < rows; i++) {
+        double calculated_b = 0.0;
+
         printf("Row %d: ", i + 1);
         for (int j = 0; j < rows; j++) {
-            printf("%.*lf * x[%d] ", precision, A[i][j], j + 1);
-            if (j < rows - 1) {
-                printf("+ ");
+            double product = A[i][j] * x[j];
+            calculated_b += product;
+
+            if (j > 0) {
+                printf(" + ");
             }
+
+            //форматування множників із дужками
+            printf("%.*lf*(%.*lf)", precision, A[i][j], precision, x[j]);
         }
-        printf("= %.*lf\n", precision, b[i]);
+
+        printf(" = %.*lf\n", precision, calculated_b);
     }
 }
 
